d5be8ad9284ac200067e63997f754984c3843d57
Corpuscle <Corpuscle@MacBook-Air-Corpuscle.local>
2017-02-19 17:57:06 +0300

    feedback updated

--- feedback/models.py
+++ feedback/models.py
@@ -10,7 +10,7 @@ class Feedback(models.Model):
 
     user = models.ForeignKey(User, verbose_name=u'Пользователь')
     discipline = models.ForeignKey(Discipline, verbose_name=u'Дисциплина')
-    question_1 = models.TextField(u'Первый вопрос?', null=True, blank=True)
+    question_1 = models.TextField(u'Предложения по содержанию курса:', null=True, blank=True)
     question_2 = models.TextField(u'Второй вопрос?', null=True, blank=True)
     question_3 = models.TextField(u'Третий вопрос???', null=True, blank=True)
 
