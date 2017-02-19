33236216ee9f67ac1dc524cb683c79cc2d0b59ca
Corpuscle <Corpuscle@MacBook-Air-Corpuscle.local>
2017-02-19 22:53:51 +0300

    url admin (application/urls.py)

--- application/urls.py
+++ application/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/300/', admin.site.urls),
     url(r'^', include('core.urls', 'core')),
     url(r'^discipline/', include('learning.urls', 'learning')),
     url(r'^feedback/', include('feedback.urls', 'feedback'))
